80bec32f38739f119948165ddc62f1fc90d1ea57
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-24 19:04:38 -0300

    Order, Instant, ISO8601

--- webservice/src/main/java/com/srdevpereira/webservice/entities/User.java
+++ webservice/src/main/java/com/srdevpereira/webservice/entities/User.java
@@ -1,8 +1,11 @@
 package com.srdevpereira.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -15,6 +18,10 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
     public User(Long id, String name, String email, String phone, String password) {
@@ -56,7 +63,9 @@ public class User implements Serializable{
     public void setPassword(String password) {
         this.password = password;
     }
-
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     @Override
     public boolean equals(Object o) {
