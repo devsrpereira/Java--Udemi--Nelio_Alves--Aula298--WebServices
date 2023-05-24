c72157ca69b4efa364f6fca1bae75c0f19cfee9e
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-24 19:42:18 -0300

    OrderStatus Enum

--- webservice/src/main/java/com/srdevpereira/webservice/entities/Order.java
+++ webservice/src/main/java/com/srdevpereira/webservice/entities/Order.java
@@ -1,6 +1,7 @@
 package com.srdevpereira.webservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.srdevpereira.webservice.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import jakarta.persistence.ManyToOne;
 
@@ -24,34 +25,41 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
-    public Order(){}
+    private Integer orderStatus;
 
-    public Order(Long id, Instant moment, User client) {
+
+    public Order(){}
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
+
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Instant getMoment() {
         return moment;
     }
-
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
-
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
     public User getClient() {
         return client;
     }
-
     public void setClient(User client) {
         this.client = client;
     }
