cdaa1079edd1cc0a4426b114f5170d3e22d24475
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-24 16:31:45 -0300

    camada Service criada e o registro do componente

--- webservice/src/main/java/com/srdevpereira/webservice/resources/UserResource.java
+++ webservice/src/main/java/com/srdevpereira/webservice/resources/UserResource.java
@@ -2,18 +2,33 @@ package com.srdevpereira.webservice.resources;
 
 
 import com.srdevpereira.webservice.entities.User;
+import com.srdevpereira.webservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.config.RepositoryNameSpaceHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Saulo", "saulo@gmail.com", "9999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> finById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
