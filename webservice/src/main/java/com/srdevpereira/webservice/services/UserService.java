da36410c79ddba563330cf4a9f365c8aa2a2f1f7
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-25 21:12:57 -0300

    Exception Handling

--- webservice/src/main/java/com/srdevpereira/webservice/services/UserService.java
+++ webservice/src/main/java/com/srdevpereira/webservice/services/UserService.java
@@ -2,8 +2,11 @@ package com.srdevpereira.webservice.services;
 
 import com.srdevpereira.webservice.entities.User;
 import com.srdevpereira.webservice.repositories.UserRepository;
+import com.srdevpereira.webservice.services.exceptions.DataBaseException;
 import com.srdevpereira.webservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+           throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+           throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
---
+++ webservice/src/main/java/com/srdevpereira/webservice/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.srdevpereira.webservice.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
